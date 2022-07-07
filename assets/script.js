73d3969d6e2755f8cbd5abf83e07d8581c761656
C K <ctkoo@ucsc.edu>
2022-07-07 00:31:24 -0700

    5-day render implemented

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,3 @@
-// moment.js: get future weather
 var APIKeyW = "c0e47fe548ceaef11cb6d43a28ec8f45";
 
 var currentDate = moment().format("M/D/YYYY");
@@ -12,12 +11,17 @@ searchButton.addEventListener("click", function(event){
     event.preventDefault();
     var citySearch = cityInput.value;
     var cityName = document.querySelector("#city-name");
-    cityName.textContent = citySearch;
+    cityName.textContent = citySearch + " ("+currentDate+")";
     cityValue(citySearch);
 });
 
-// API functions
+//For debug purposes
+.addEventListener("click",function() {
+    searchHistory = [];
+    renderSearchHistory();
+}
 
+// API functions
 // Function to fetch the geolocation 
 function cityValue(citySearch){
     fetch("http://api.openweathermap.org/geo/1.0/direct?q="
@@ -55,22 +59,46 @@ function cityValue(citySearch){
         var getBurnt = data2.current.uvi;
         console.log(getBurnt);
         var alertUV = document.createElement("span");
+        alertUV.setAttribute("class","safe rounded");
+        if(getBurnt>2){
+            alertUV.setAttribute("class","danger rounded");
+        }
         alertUV.textContent = getBurnt;
         currentUV.append(alertUV);
-        if(getBurnt>2){
-            alertUV.setAttribute("class","danger");
-        }else{
-            alertUV.setAttribute("class","safe");
-        }
 
+        var dayFore = document.getElementById("5-day");
+
+        // For loop for 5 day forecast
         for(var i=0;i<5;i++){
-            
-        }
+            var day = document.createElement("div");
+            day.setAttribute("class","col-md-3 forecast bg-primary text-white rounded");
+            dayFore.appendChild(day);
 
+            var thisDay = document.createElement("h5");
+            thisDay.textContent = moment().add(i+1, 'days').format("M/D/YYYY");
+            day.append(thisDay);
+
+            var imageW = document.createElement("img");
+            var iconW = data2.daily[i].weather[0].icon;
+            imageW.setAttribute("src","http://openweathermap.org/img/wn/"+iconW+"@2x.png");
+            day.append(imageW);
+            
+            var thisTemp = (data2.daily[i].temp.max+data2.daily[i].temp.min)/2;
+            thisTemp = Math.round(thisTemp*100)/100;
+            var printTemp = document.createElement("p");
+            printTemp.textContent = "Temp: "+thisTemp+" ºF";
+            thisDay.append(printTemp);
+
+            var thisWind = data2.daily[i].wind_speed;
+            var printWind = document.createElement("p");
+            printWind.textContent = "Wind: "+thisWind+" MPH";
+            thisDay.append(printWind);
+
+            var thisWet = data2.daily[i].humidity;
+            var printWet = document.createElement("p");
+            printWet.textContent = "Humidity: "+thisWet+"%";
+            thisDay.append(printWet);
+        }
       })
-      
     });
 }
-
-// for loop for 5 day forecast
-
